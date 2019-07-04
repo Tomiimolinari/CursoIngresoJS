ffc2f840a00c30266540d61f63b49d35e21b900c
Tomás Molinari <52511458+Tomiimolinari@users.noreply.github.com>
2019-07-03 21:18:42 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,8 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	//Remplazamos lo que queremos que se muestre
+	alert("Esto funciona de maravilla"); // alert muestra una ventana.
+	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    var nombre; //"nombre" significa el nombre que le ponemos a el espacio asignado para guardar los datos
+
+    nombre=prompt("ingrese nombre"); /* prompt crea una ventana para ingresar el valor que le vamos a 
+                                        asignar a "nombre" (variable)"ingrese nombre" el texto a mostrar */
+
+    alert (nombre); //alert ejecuta una ventana mostrando el valor dado anteriormente a la variable (nombre)
+
+
 }
 
